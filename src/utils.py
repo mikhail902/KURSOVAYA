a2a86c38bf24cb974fb2d3c688679a242dc71591
миша <misatoropkov@gmail.com>
2025-01-28 16:46:42 +0300

    some fixes 21.01

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,59 @@
 import datetime
+import os
 
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+API_KEY = os.getenv("API_KEY")
 PATH_TO_EXCEL = "C:/Users/Sator/PycharmProjects/KURSOVAYA/data/operations.xlsx"
+
+
+def conversion(dictionary: dict) -> any:
+    """Считывания курса валюты через API"""
+    try:
+        url = "https://api.apilayer.com/exchangerates_data/convert"
+        payload = {
+            "amount": dictionary["operationAmount"]["amount"],
+            "from": dictionary["operationAmount"]["currency"]["code"],
+            "to": "RUB",
+        }
+        headers = {"apikey": API_KEY}
+        response = requests.get(url, headers=headers, params=payload).json()
+        return response["result"]
+    except KeyError:
+        return "Ошибка конвертации, закончился лимит запросов на перевод валюты"
+
+
+def excel_transaction(file_path: str) -> list:
+    """Функция считывающая строки из эксель файла и превращающая в список словарей из строк файла"""
+    try:
+        df = pd.read_excel(file_path)
+        list_of_dicts = df.to_dict(orient="records")
+        return list_of_dicts
+    except FileNotFoundError:
+        print(f"Ошибка: Файл не найден по пути: {file_path}")
+        return []
+    except Exception as e:
+        print(f"Ошибка при чтении файла: {e}")
+        return []
+
+
+def get_transactions_by_card(file_path: str) -> dict:
+    """Вывод из экселя"""
+    df = pd.read_excel(file_path)
+    transactions_by_card = {}
+    for index, row in df.iterrows():
+        card_number = str(row["Номер карты"])
+        transaction_amount = float(row["Сумма операции с округлением"])
+        if card_number in transactions_by_card:
+            if card_number != "nan":
+                transactions_by_card[card_number] += transaction_amount
+        else:
+            if card_number != "nan":
+                transactions_by_card[card_number] = transaction_amount
+    return transactions_by_card
 
 
 def time_of_day():
@@ -8,20 +61,50 @@ def time_of_day():
     time_now = datetime.datetime.now()
     h = int(time_now.strftime("%H"))
     if h < 18 | h > 12:
-        now_time = "день"
+        now_time = "Добрый день"
     elif h > 18 | h < 22:
-        now_time = "вечер"
+        now_time = "Добрый вечер"
     elif h > 4 | h < 12:
-        now_time = "утро"
+        now_time = "Доброе утро"
     else:
-        now_time = "ночи"
+        now_time = "Доброй ночи"
     return now_time
 
 
-def sort_transactions(excel_file: list) -> list:
-    """Функция поиска топ 5 платежей"""
-    sorted_list = sorted(excel_file, key=lambda x: x["Сумма операции"], reverse=False)
-    return sorted_list[0:6]
+def sort_transactions_by_amount(transactions_dict: dict, reverse=True) -> list:
+    """Функция сортировки и вывод платежей"""
+    new_list = [{}, {}, {}, {}, {}]
+    k = 0
+    sorted_transactions = dict(
+        sorted(transactions_dict.items(), key=lambda item: item[1], reverse=reverse)
+    )
+    for i, j in sorted_transactions.items():
+        new_list[k]["last_digit"] = i
+        new_list[k]["total_spent"] = j
+        new_list[k]["cashback"] = j / 100
+        k += 1
+        if k > 4:
+            break
+    return new_list
 
-def last_four_num()->:
-    pass
+
+def sort_list_of_dictionaries(path, key="Сумма операции", reverse=False):
+    """Функция сортировки и вывод топ 5 платежей"""
+    new_list = []
+    some_dict = {}
+    list_of_dicts = excel_transaction(path)
+    sorted_list = sorted(
+        list_of_dicts, key=lambda item: item.get(key, 0), reverse=reverse
+    )
+    for k in sorted_list:
+        for i, j in k.items():
+            if (
+                i == "Дата платежа"
+                or i == "Сумма операции"
+                or i == "Категория"
+                or i == "Описание"
+            ):
+                some_dict[i] = j
+        new_list.append(some_dict)
+        new_list = new_list[:5]
+    return new_list
--- src/views.py
+++ src/views.py
@@ -1,5 +1,35 @@
 import json
 
+from src.utils import *
 
-def home(excel_list:list)->any:
-    """Функция по работе с главной старницой"""
+PATH_TO_EXCEL = "C:/Users/Sator/PycharmProjects/KURSOVAYA/data/operations.xlsx"
+PATH_TO_JSON = "C:/Users/Sator/PycharmProjects/KURSOVAYA/data/answer.json"
+
+
+def home(url:str)->any:
+    """Основная логика страницы главная"""
+    try:
+        a = get_transactions_by_card(PATH_TO_EXCEL)
+        b = sort_list_of_dictionaries(PATH_TO_EXCEL)
+        with open(url, "w", encoding="utf-8") as f:
+            times = time_of_day()
+            data = {
+                "greeting": times,
+                "cards": sort_transactions_by_amount(a),
+                "top_transactions": b,
+                "currency_rates": {"currency": " ", "rate": " "},
+                "stock_prices": {},
+            }
+            json.dump(data, f, indent=4)
+            return data
+    except (FileNotFoundError, TypeError, json.JSONDecodeError) as e:
+        return []
+
+def events()->any:
+    """Основная логика страницы события"""
+    pass
+
+
+if __name__ == "__main__":
+    home(PATH_TO_JSON)
+    print("Ответ успешно записан в JSON-файл!")
